ad25c8cf4eaa58906f7e03c3730888d1d7ecd5fc
J-P Nurmi <jpnurmi@gmail.com>
2021-07-18 21:55:48 +0200

    Add docs

--- packages/wizard_router/lib/src/wizard.dart
+++ packages/wizard_router/lib/src/wizard.dart
@@ -2,9 +2,112 @@ import 'package:flutter/material.dart';
 import 'package:flow_builder/flow_builder.dart';
 import 'package:provider/provider.dart';
 
+/// The signature of [Wizard.onNext] and [Wizard.onBack] callbacks.
 typedef WizardRouteCallback = String? Function(RouteSettings settings);
 
+/// A wizard is a flow of steps that the user can navigate through.
+///
+/// `Wizard` provides routing for classic linear wizards in a way that it
+/// eliminates dependencies between wizard pages. Wizard pages can request the
+/// next or previous page without knowing or caring what is the next or the
+/// previous wizard page. Thus, adding, removing, or re-ordering pages does not
+/// cause changes in existing pages.
+///
+/// ![wizard_router](https://github.com/jpnurmi/wizard_router/raw/main/images/wizard_router.png)
+///
+/// ## Usage
+///
+/// ### Routes
+///
+/// ```dart
+/// MaterialApp(
+///   home: Scaffold(
+///     body: Wizard(
+///       routes: {
+///         '/foo': (context) => FooPage(),
+///         '/bar': (context) => BarPage(),
+///         '/baz': (context) => BazPage(),
+///       },
+///     ),
+///   ),
+/// )
+/// ```
+///
+/// ### Navigation
+///
+/// The next or the previous page is requested by calling `Wizard.of(context).next()`
+/// or `Wizard.of(context).back()`, respectively.
+///
+/// ```dart
+/// BarPage(
+///   child: ButtonBar(
+///     children: [
+///       ElevatedButton(
+///         onPressed: Wizard.of(context).back
+///         child: const Text('Back'),
+///       ),
+///       ElevatedButton(
+///         onPressed: Wizard.of(context).next
+///         child: const Text('Next'),
+///       ),
+///     ],
+///   ),
+/// )
+/// ```
+///
+/// ### Conditions
+///
+/// For unconditional linear wizards, defining the routes is enough. The router
+/// follows the order the routes are defined in. If there are conditions between
+/// the wizard pages, the order can be customized with the `Wizard.onNext` and
+/// `Wizard.onBack` callbacks.
+///
+/// ```dart
+/// Wizard(
+///   routes: {
+///     '/foo': (context) => FooPage(),
+///     '/bar': (context) => BarPage(),
+///     '/baz': (context) => BazPage(),
+///     '/qux': (context) => QuxPage(),
+///   },
+///   onNext: (settings) {
+///     // conditionally skip the _Bar_ page when stepping forward from the _Foo_ page
+///     if (settings.name == '/foo' && skipBar) return '/baz';
+///     return null;
+///   }
+///   onBack: (settings) {
+///     // always skip the Baz page when stepping back from the Qux page
+///     if (settings.name == '/qux') return '/bar';
+///     return null;
+///   }
+/// )
+/// ```
+///
+/// ### Arguments
+///
+/// It is recommended to avoid such dependencies between wizard pages that make
+/// assumptions of the page order. However, sometimes it may be desirable to pass
+/// arguments to the next page. This is possible by passing them to
+/// `Wizard.of(context).next(arguments)`. On the next page, the arguments can be
+/// queried from `Wizard.of(context).arguments`.
+///
+/// ```dart
+/// FooPage(
+///   onFoo: () => Wizard.of(context).next(arguments: something),
+/// )
+///
+/// BarPageState extends State<BarPage>(
+///   @override
+///   void initState() {
+///     super.initState();
+///
+///     final something = Wizard.of(context).arguments as Something;
+///     // ...
+///   }
+/// )
+/// ```
 class Wizard extends StatefulWidget {
+  /// Creates an instance of a wizard. The `routes` argument is required.
   const Wizard({
     Key? key,
     this.initialRoute,
@@ -13,11 +116,37 @@ class Wizard extends StatefulWidget {
     this.onBack,
   }) : super(key: key);
 
+  /// The name of the first route to show.
+  ///
+  /// If `initialRoute` not specified, the first route from [routes] is
+  /// considered the initial route.
   final String? initialRoute;
+
+  /// The wizards's routing table.
+  ///
+  /// The order of `routes` is the order of the wizard pages are shown. The
+  /// order can be customized with [onNext] and [onBack].
   final Map<String, WidgetBuilder> routes;
+
+  /// The callback invoked when the next page is requested.
+  ///
+  /// If `onNext` is not specified or it returns `null`, the order is determined
+  /// from [routes].
   final WizardRouteCallback? onNext;
+
+  /// The callback invoked when the previous page is requested.
+  ///
+  /// If `onBack` is not specified or it returns `null`, the order is determined
+  /// from [routes].
   final WizardRouteCallback? onBack;
 
+  /// The wizard scope from the closest instance of this class that encloses the
+  /// given `context`.
+  ///
+  /// See also:
+  /// - [WizardScopeState.next]
+  /// - [WizardScopeState.arguments]
+  /// - [WizardScopeState.back]
   static WizardScopeState of(BuildContext context) {
     return Provider.of<WizardScopeState>(context, listen: false);
   }
@@ -71,36 +200,54 @@ class _WizardState extends State<Wizard> {
   }
 }
 
+/// The scope of a wizard page.
+///
+/// Each page is enclosed by a `WizardScope` widget.
 class WizardScope extends StatefulWidget {
   const WizardScope({
     Key? key,
-    required this.child,
-    required this.routes,
-    this.onNext,
-    this.onBack,
-  }) : super(key: key);
+    required Widget child,
+    required List<String> routes,
+    WizardRouteCallback? onNext,
+    WizardRouteCallback? onBack,
+  })  : _child = child,
+        _routes = routes,
+        _onNext = onNext,
+        _onBack = onBack,
+        super(key: key);
 
-  final Widget child;
-  final List<String> routes;
-  final WizardRouteCallback? onNext;
-  final WizardRouteCallback? onBack;
+  final Widget _child;
+  final List<String> _routes;
+  final WizardRouteCallback? _onNext;
+  final WizardRouteCallback? _onBack;
 
   @override
   State<WizardScope> createState() => WizardScopeState();
 }
 
+/// The state of a `WizardScope`, accessed via `Wizard.of(context)`.
 class WizardScopeState extends State<WizardScope> {
+  /// Arguments passed from the previous page.
+  ///
+  /// ```dart
+  /// final something = Wizard.of(context).arguments as Something;
+  /// ```
   Object? get arguments => ModalRoute.of(context)?.settings.arguments;
 
+  /// Requests the wizard to show the previous page.
+  ///
+  /// ```dart
+  /// onPressed: Wizard.of(context).back
+  /// ```
   void back() {
     final routes = context.flow<List<RouteSettings>>().state;
     assert(routes.length > 1,
         '`Wizard.back()` called from the first route ${routes.last.name}');
 
     // go back to a specific route, or pick the previous route on the list
-    final previous = widget.onBack?.call(routes.last);
+    final previous = widget._onBack?.call(routes.last);
     if (previous != null) {
-      assert(widget.routes.contains(previous),
+      assert(widget._routes.contains(previous),
           '`Wizard.routes` is missing route \'$previous\'.');
     }
 
@@ -114,6 +261,12 @@ class WizardScopeState extends State<WizardScope> {
     });
   }
 
+  /// Requests the wizard to show the next page. Optionally, `arguments` can be
+  /// passed to the next page.
+  ///
+  /// ```dart
+  /// onPressed: Wizard.of(context).next
+  /// ```
   void next({Object? arguments}) {
     final routes = context.flow<List<RouteSettings>>().state;
     assert(routes.isNotEmpty, routes.length.toString());
@@ -121,14 +274,14 @@ class WizardScopeState extends State<WizardScope> {
     final previous = routes.last.copyWith(arguments: arguments);
 
     // advance to a specific route
-    String? onNext() => widget.onNext?.call(previous);
+    String? onNext() => widget._onNext?.call(previous);
 
     // pick the next route on the list
     String nextRoute() {
-      final index = widget.routes.indexOf(previous.name!);
-      assert(index < widget.routes.length - 1,
+      final index = widget._routes.indexOf(previous.name!);
+      assert(index < widget._routes.length - 1,
           '`Wizard.next()` called from the last route ${previous.name}.');
-      return widget.routes[index + 1];
+      return widget._routes[index + 1];
     }
 
     final next = RouteSettings(
@@ -136,7 +289,7 @@ class WizardScopeState extends State<WizardScope> {
       arguments: arguments,
     );
 
-    assert(widget.routes.contains(next.name),
+    assert(widget._routes.contains(next.name),
         '`Wizard.routes` is missing route \'${next.name}\'.');
 
     context.flow<List<RouteSettings>>().update((state) {
@@ -149,7 +302,7 @@ class WizardScopeState extends State<WizardScope> {
   Widget build(BuildContext context) {
     return Provider<WizardScopeState>.value(
       value: this,
-      child: widget.child,
+      child: widget._child,
     );
   }
 }
--- packages/wizard_router/lib/wizard_router.dart
+++ packages/wizard_router/lib/wizard_router.dart
@@ -1,3 +1,10 @@
+/// This library provides routing for classic linear wizards in a way that it
+/// eliminates dependencies between wizard pages. Wizard pages can request the
+/// next or previous page without knowing or caring what is the next or the
+/// previous wizard page. Thus, adding, removing, or re-ordering pages does not
+/// cause changes in existing pages.
+///
+/// ![wizard_router](https://github.com/jpnurmi/wizard_router/raw/main/images/wizard_router.png)
 library wizard_router;
 
 export 'src/wizard.dart';
