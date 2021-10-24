381e2255374d3af04fc1188f17930e6abb80a5be
J-P Nurmi <jpnurmi@gmail.com>
2021-10-24 18:44:22 +0200

    Add example stub

---
+++ example/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:xdg_icons/xdg_icons.dart';
+import 'package:yaru/yaru.dart' as yaru;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: yaru.lightTheme,
+      darkTheme: yaru.darkTheme,
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('XDG icons')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
+}
