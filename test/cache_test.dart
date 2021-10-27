4c3d33cc161d9b33aa6e24335387ddd8a4c90994
J-P Nurmi <jpnurmi@gmail.com>
2021-10-27 22:11:29 +0200

    Test cache

--- test/cache_test.dart
+++ test/cache_test.dart
@@ -1,23 +1,63 @@
+import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:xdg_icons/src/cache.dart';
 
 void main() {
-  test('yaru', () {
-    final cache = XdgIconCache('/usr/share/icons/Yaru/icon-theme.cache');
+  test('invalid', () {
+    final cache = XdgIconCache('foo/bar/icon-theme.cache');
+    expect(cache.isValid, isFalse);
+    expect(cache.lookup('computer'), isEmpty);
+  });
+
+  test('outdated', () {
+    final file = File('test/data/icon-theme.cache');
+    final modified = file.parent.statSync().modified;
+    file.setLastModifiedSync(modified.subtract(const Duration(hours: 1)));
+    final cache = XdgIconCache('test/data/icon-theme.cache');
+    expect(cache.isValid, isFalse);
+    expect(cache.lookup('computer'), isEmpty);
+  });
+
+  test('valid', () {
+    File('test/data/icon-theme.cache').setLastModifiedSync(DateTime.now());
+    final cache = XdgIconCache('test/data/icon-theme.cache');
     expect(cache.isValid, isTrue);
+
     expect(
-        cache.lookup('computer'),
-        equals([
-          '48x48@2x/devices',
-          '48x48/devices',
-          '32x32@2x/devices',
-          '32x32/devices',
-          '256x256@2x/devices',
-          '256x256/devices',
-          '24x24@2x/devices',
-          '24x24/devices',
-          '16x16@2x/devices',
-          '16x16/devices',
-        ]));
+      cache.lookup('computer'),
+      equals([
+        'scalable/devices',
+        '48x48/devices',
+        '32x32/devices',
+        '256x256/devices',
+        '24x24/devices',
+        '22x22/devices',
+        '16x16/devices',
+      ]),
+    );
+    expect(
+      cache.lookup('folder'),
+      equals([
+        'scalable/places',
+        '48x48/places',
+        '32x32/places',
+        '256x256/places',
+        '24x24/places',
+        '22x22/places',
+        '16x16/places',
+      ]),
+    );
+    expect(
+      cache.lookup('web-browser'),
+      equals([
+        '48x48/apps',
+        '32x32/apps',
+        '256x256/apps',
+        '24x24/apps',
+        '22x22/apps',
+        '16x16/apps',
+      ]),
+    );
   });
 }
