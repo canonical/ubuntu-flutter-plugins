0aef69706e04487aace8da796110705daa092e8e
J-P Nurmi <jp.nurmi@canonical.com>
2021-11-15 15:00:30 +0100

    Add test for localizations

---
+++ packages/ubuntu_localizations/test/localizations_test.dart
@@ -0,0 +1,37 @@
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ubuntu_localizations/src/material/material_localizations.dart';
+import 'package:ubuntu_localizations/ubuntu_localizations.dart';
+
+// TODO: implement
+const unsupportedLocales = ['bo', 'dz', 'ku'];
+
+void main() {
+  testWidgets('material supports all ubuntu localizations', (tester) async {
+    final delegates = [
+      GlobalMaterialLocalizations.delegate,
+      ...UbuntuMaterialLocalizations.delegates,
+    ];
+
+    for (final locale in UbuntuLocalizations.supportedLocales) {
+      // TODO: remove
+      if (unsupportedLocales.contains(locale.languageCode)) continue;
+
+      expect(
+        delegates.any((delegate) => delegate.isSupported(locale)),
+        isTrue,
+        reason:
+            'MaterialLocalizations for "$locale" not found. Is the delegate is missing?',
+      );
+
+      final delegate =
+          delegates.firstWhere((delegate) => delegate.isSupported(locale));
+      await expectLater(
+        () async => await delegate.load(locale),
+        returnsNormally,
+        reason:
+            'MaterialLocalizations failed to load "$locale". Missing date time patters/symbols?',
+      );
+    }
+  });
+}
