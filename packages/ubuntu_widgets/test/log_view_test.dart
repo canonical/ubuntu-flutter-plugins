161b9669c3ec66eb5a95ffc6aff68f06984b3269
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2022-01-18 12:00:52 -0300

    Added test to verify user scroll after lines added

--- packages/ubuntu_widgets/test/log_view_test.dart
+++ packages/ubuntu_widgets/test/log_view_test.dart
@@ -42,4 +42,52 @@ void main() {
     expect(scrollController.position.extentAfter, equals(0.0));
     expect(scrollController.position.maxScrollExtent, greaterThan(0.0));
   });
+
+  testWidgets('user scroll not affected by appended lines', (tester) async {
+    final log = StreamController<String>(sync: true);
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: LogView(log: log.stream, maxLines: 2),
+        ),
+      ),
+    );
+
+    final textField = tester.widget<TextField>(find.byType(TextField));
+
+    final controller = textField.controller;
+    expect(controller, isNotNull);
+
+    final scrollController = textField.scrollController;
+    expect(scrollController, isNotNull);
+
+    for (int i = 1; i < 6; i++) {
+      log.add('line $i');
+      await tester.pump();
+    }
+    expect(controller!.text, equals('line 1\nline 2\nline 3\nline 4\nline 5'));
+    expect(scrollController!.offset, greaterThan(0.0));
+    expect(scrollController.position.extentAfter, equals(0.0));
+    expect(scrollController.position.maxScrollExtent, greaterThan(0.0));
+
+    // moving scroll manually
+    scrollController.jumpTo(0.0);
+    await tester.pump();
+    _scrollOffsetShouldStayAt0(scrollController);
+
+    log.add('line 6');
+    await tester.pump();
+    expect(
+      controller.text,
+      equals('line 1\nline 2\nline 3\nline 4\nline 5\nline 6'),
+    );
+    _scrollOffsetShouldStayAt0(scrollController);
+  });
+}
+
+void _scrollOffsetShouldStayAt0(ScrollController scrollController) {
+  expect(scrollController.offset, equals(0.0));
+  expect(scrollController.position.extentAfter, greaterThan(0.0));
+  expect(scrollController.position.maxScrollExtent, greaterThan(0.0));
 }
