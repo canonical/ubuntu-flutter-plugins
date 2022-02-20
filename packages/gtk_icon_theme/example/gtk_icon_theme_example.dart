cf211433b6e2d3ac57435ec4ddd6289729c20690
J-P Nurmi <jpnurmi@gmail.com>
2022-02-20 15:33:40 +0100

    Add simple Dart package template

---
+++ packages/gtk_icon_theme/example/gtk_icon_theme_example.dart
@@ -0,0 +1,6 @@
+import 'package:gtk_icon_theme/gtk_icon_theme.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/gtk_icon_theme/test/gtk_icon_theme_test.dart
@@ -0,0 +1,16 @@
+import 'package:gtk_icon_theme/gtk_icon_theme.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
