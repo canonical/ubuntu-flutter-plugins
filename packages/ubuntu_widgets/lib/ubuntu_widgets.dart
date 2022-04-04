e07ff0c58217433a4667a0bad54d593b6be28580
J-P Nurmi <jp.nurmi@canonical.com>
2022-04-04 23:34:11 +0200

    Check & radio buttons
    CheckButton and RadioButton have been promoted from ubuntu_wizard to
    ubuntu_widgets. They have been generalized, and gained proper layout
    calculation, accessibility, and theming support.

--- packages/ubuntu_widgets/lib/ubuntu_widgets.dart
+++ packages/ubuntu_widgets/lib/ubuntu_widgets.dart
@@ -1,5 +1,9 @@
 /// A collection of widgets developed for Flutter-based Ubuntu applications.
 library ubuntu_widgets;
 
+export 'src/check_button.dart';
 export 'src/log_view.dart';
+export 'src/radio_button.dart';
 export 'src/slide_show.dart';
+export 'src/toggle_button.dart';
+export 'src/toggle_button_theme.dart';
