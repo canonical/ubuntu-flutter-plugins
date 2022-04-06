f2cc95b2e33e758ce83e1711eea6998ce8ba302d
J-P Nurmi <jp.nurmi@canonical.com>
2022-04-06 09:08:37 +0200

    Import DropdownBuilder and MenuButtonBuilder (#52)
    Generic theme-agnostic builders from udi/ubuntu_wizard.

--- packages/ubuntu_widgets/lib/ubuntu_widgets.dart
+++ packages/ubuntu_widgets/lib/ubuntu_widgets.dart
@@ -2,7 +2,9 @@
 library ubuntu_widgets;
 
 export 'src/check_button.dart';
+export 'src/dropdown_builder.dart';
 export 'src/log_view.dart';
+export 'src/menu_button_builder.dart';
 export 'src/radio_button.dart';
 export 'src/slide_show.dart';
 export 'src/toggle_button.dart';
---
+++ packages/ubuntu_widgets/test/dropdown_builder_test.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ubuntu_widgets/ubuntu_widgets.dart';
+
+enum TestEnum { foo, bar, baz }
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets('builds an item for each value', (tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Material(
+          child: DropdownBuilder<TestEnum>(
+            values: TestEnum.values,
+            onSelected: (_) {},
+            itemBuilder: (_, value, __) => Text(value.toString()),
+          ),
+        ),
+      ),
+    );
+
+    for (final value in TestEnum.values) {
+      expect(find.text(value.toString()), findsOneWidget);
+    }
+  });
+
+  testWidgets('selects initial value', (tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Material(
+          child: DropdownBuilder<TestEnum>(
+            selected: TestEnum.baz,
+            values: TestEnum.values,
+            onSelected: (value) {},
+            itemBuilder: (_, value, __) => Text(value.toString()),
+          ),
+        ),
+      ),
+    );
+
+    final stack = tester.widget<IndexedStack>(find.byType(IndexedStack));
+    expect(stack.index, equals(2));
+  });
+
+  testWidgets('selects tapped value', (tester) async {
+    TestEnum? selectedValue;
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Material(
+          child: DropdownBuilder<TestEnum>(
+            values: TestEnum.values,
+            onSelected: (value) => selectedValue = value,
+            itemBuilder: (_, value, __) => Text(value.toString()),
+          ),
+        ),
+      ),
+    );
+
+    await tester.tap(find.byType(DropdownButton<TestEnum>));
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.text(TestEnum.bar.toString()).last);
+    await tester.pumpAndSettle();
+
+    expect(selectedValue, equals(TestEnum.bar));
+  });
+
+  testWidgets('allows null value', (tester) async {
+    int? selectedValue = -1;
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Material(
+          child: DropdownBuilder<int?>(
+            values: const [1, 2, 3, null],
+            onSelected: (value) => selectedValue = value,
+            itemBuilder: (_, value, __) => Text(value.toString()),
+          ),
+        ),
+      ),
+    );
+
+    await tester.tap(find.byType(DropdownButton<int?>));
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.text('null').last);
+    await tester.pumpAndSettle();
+
+    expect(selectedValue, isNull);
+  });
+}
