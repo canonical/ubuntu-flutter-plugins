8160d96b7009bf4c03735cae21a40e9c96bcd0bc
jpnurmi <jpnurmi@users.noreply.github.com>
2022-04-27 05:50:52 +0000

    Regenerate files

--- packages/ubuntu_localizations/lib/src/l10n/ubuntu_localizations_hu.dart
+++ packages/ubuntu_localizations/lib/src/l10n/ubuntu_localizations_hu.dart
@@ -8,26 +8,26 @@ class UbuntuLocalizationsHu extends UbuntuLocalizations {
   UbuntuLocalizationsHu([String locale = 'hu']) : super(locale);
 
   @override
-  String get countryCode => 'HU';
+  String get countryCode => 'US';
 
   @override
-  String get languageName => 'Magyar';
+  String get languageName => 'Angol';
 
   @override
-  String get backAction => 'Go Back';
+  String get backAction => 'Vissza';
 
   @override
-  String get continueAction => 'Continue';
+  String get continueAction => 'Folytatás';
 
   @override
-  String get strongPassword => 'Strong password';
+  String get strongPassword => 'Erős jelszó';
 
   @override
-  String get fairPassword => 'Fair password';
+  String get fairPassword => 'Korrekt jelszó';
 
   @override
-  String get goodPassword => 'Good password';
+  String get goodPassword => 'Jó jelszó';
 
   @override
-  String get weakPassword => 'Weak password';
+  String get weakPassword => 'Gyenge jelszó';
 }
