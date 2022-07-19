803edc77ab283696d2852f5e80ad359c58a2c873
J-P Nurmi <jp.nurmi@canonical.com>
2022-07-19 23:00:19 +0200

    Fix analysis

--- test/icon_test.dart
+++ test/icon_test.dart
@@ -1,8 +1,6 @@
 import 'dart:async';
 import 'dart:io';
-import 'dart:typed_data';
 
-import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_test/flutter_test.dart';
