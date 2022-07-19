10d46451718da8378f6196f56058de7c894b70a2
J-P Nurmi <jp.nurmi@canonical.com>
2022-07-19 22:55:56 +0200

    Add icon file & data tests

--- test/icon_test.dart
+++ test/icon_test.dart
@@ -1,8 +1,13 @@
 import 'dart:async';
+import 'dart:io';
+import 'dart:typed_data';
 
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:path/path.dart' as path;
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 import 'package:xdg_icons/src/data.dart';
 import 'package:xdg_icons/src/platform_interface.dart';
@@ -264,6 +269,144 @@ void main() {
     verify(() => mock.lookupIcon(name: 'foo', size: 24)).called(1);
     themeChange.add(null);
     verify(() => mock.lookupIcon(name: 'foo', size: 24)).called(1);
+  });
+
+  testWidgets('png file', (tester) async {
+    final fileName = path.join(Directory.current.path, 'test', 'red.png');
+
+    final mock = MockXdgIconsPlatform();
+    when(() => mock.lookupIcon(name: 'red', size: 16))
+        .thenAnswer((_) async => XdgIconData(
+              baseScale: 1,
+              baseSize: 16,
+              fileName: fileName,
+              isSymbolic: false,
+            ));
+    when(() => mock.onDefaultThemeChanged)
+        .thenAnswer((_) => const Stream<dynamic>.empty());
+    XdgIconsPlatform.instance = mock;
+
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: XdgIcon(
+              name: 'red',
+              size: 16,
+            ),
+          ),
+        ),
+      ),
+    );
+    await tester.pumpAndSettle();
+
+    expect(find.image(FileImage(File(fileName))), findsOneWidget);
+  });
+
+  testWidgets('png data', (tester) async {
+    final fileName = path.join(Directory.current.path, 'test', 'red.png');
+    final bytes = File(fileName).readAsBytesSync();
+
+    final mock = MockXdgIconsPlatform();
+    when(() => mock.lookupIcon(name: 'red', size: 16))
+        .thenAnswer((_) async => XdgIconData(
+              baseScale: 1,
+              baseSize: 16,
+              fileName: '/gtk/resources/red.png',
+              data: bytes,
+              isSymbolic: false,
+            ));
+    when(() => mock.onDefaultThemeChanged)
+        .thenAnswer((_) => const Stream<dynamic>.empty());
+    XdgIconsPlatform.instance = mock;
+
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: XdgIcon(
+              name: 'red',
+              size: 16,
+            ),
+          ),
+        ),
+      ),
+    );
+    await tester.pumpAndSettle();
+
+    final widget = tester.widget<Image>(find.byType(Image));
+    expect(widget.image,
+        isA<MemoryImage>().having((p) => p.bytes, 'bytes', bytes));
+  });
+
+  testWidgets('svg file', (tester) async {
+    final fileName = path.join(Directory.current.path, 'test', 'blue.svg');
+
+    final mock = MockXdgIconsPlatform();
+    when(() => mock.lookupIcon(name: 'blue', size: 16))
+        .thenAnswer((_) async => XdgIconData(
+              baseScale: 1,
+              baseSize: 16,
+              fileName: fileName,
+              isSymbolic: false,
+            ));
+    when(() => mock.onDefaultThemeChanged)
+        .thenAnswer((_) => const Stream<dynamic>.empty());
+    XdgIconsPlatform.instance = mock;
+
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: XdgIcon(
+              name: 'blue',
+              size: 16,
+            ),
+          ),
+        ),
+      ),
+    );
+    await tester.pumpAndSettle();
+
+    final widget = tester.widget<SvgPicture>(find.byType(SvgPicture));
+    expect(widget.pictureProvider,
+        isA<FilePicture>().having((p) => p.file.path, 'file', fileName));
+  });
+
+  testWidgets('svg data', (tester) async {
+    final fileName = path.join(Directory.current.path, 'test', 'blue.svg');
+    final bytes = File(fileName).readAsBytesSync();
+
+    final mock = MockXdgIconsPlatform();
+    when(() => mock.lookupIcon(name: 'blue', size: 16))
+        .thenAnswer((_) async => XdgIconData(
+              baseScale: 1,
+              baseSize: 16,
+              fileName: '/gtk/resources/blue.svg',
+              data: bytes,
+              isSymbolic: false,
+            ));
+    when(() => mock.onDefaultThemeChanged)
+        .thenAnswer((_) => const Stream<dynamic>.empty());
+    XdgIconsPlatform.instance = mock;
+
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: XdgIcon(
+              name: 'blue',
+              size: 16,
+            ),
+          ),
+        ),
+      ),
+    );
+    await tester.pumpAndSettle();
+
+    final widget = tester.widget<SvgPicture>(find.byType(SvgPicture));
+    expect(widget.pictureProvider,
+        isA<MemoryPicture>().having((p) => p.bytes, 'bytes', bytes));
   });
 }
 
