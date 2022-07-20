6d112b9870a062e29e49a9b88d900a83e9bb7d4a
J-P Nurmi <jp.nurmi@canonical.com>
2022-07-20 22:29:08 +0200

    Example: tweak window size

--- example/linux/my_application.cc
+++ example/linux/my_application.cc
@@ -47,7 +47,7 @@ static void my_application_activate(GApplication* application) {
     gtk_window_set_title(window, "XDG Icons");
   }
 
-  gtk_window_set_default_size(window, 640, 640);
+  gtk_window_set_default_size(window, 960, 600);
   gtk_widget_show(GTK_WIDGET(window));
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();
