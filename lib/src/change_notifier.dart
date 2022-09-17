44a2a6d8e4b99b576cfbe02cb4961d9612a1cab1
J-P Nurmi <jp.nurmi@canonical.com>
2022-09-17 09:21:59 +0200

    Add SafeNotifierMixin

--- lib/src/change_notifier.dart
+++ lib/src/change_notifier.dart
@@ -1,42 +1,9 @@
 import 'package:flutter/foundation.dart';
 
+import 'notifier_mixin.dart';
+
 /// A safe drop-in replacement for Flutter's `ChangeNotifier`, that makes
 /// `notifyListeners()` a no-op, rather than an error, after its disposal.
 ///
 /// ![safe_change_notifier](https://github.com/ubuntu-flutter-community/safe_change_notifier/raw/main/images/safe_change_notifier.png)
-class SafeChangeNotifier extends ChangeNotifier {
-  var _isDisposed = false;
-
-  /// Whether the notifier has been disposed.
-  bool get isDisposed => _isDisposed;
-
-  @override
-  bool get hasListeners => !_isDisposed && super.hasListeners;
-
-  @override
-  void notifyListeners() {
-    if (!_isDisposed) {
-      super.notifyListeners();
-    }
-  }
-
-  @override
-  void addListener(VoidCallback listener) {
-    if (!_isDisposed) {
-      super.addListener(listener);
-    }
-  }
-
-  @override
-  void removeListener(VoidCallback listener) {
-    if (!_isDisposed) {
-      super.removeListener(listener);
-    }
-  }
-
-  @override
-  void dispose() {
-    _isDisposed = true;
-    super.dispose();
-  }
-}
+class SafeChangeNotifier extends ChangeNotifier with SafeNotifierMixin {}
