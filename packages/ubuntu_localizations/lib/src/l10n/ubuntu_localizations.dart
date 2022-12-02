1594c60e8e7730fcc0c11215b9bb565aefef15df
J-P Nurmi <jp.nurmi@canonical.com>
2022-12-02 16:34:37 +0100

    Regenerate l10n

--- packages/ubuntu_localizations/lib/src/l10n/ubuntu_localizations.dart
+++ packages/ubuntu_localizations/lib/src/l10n/ubuntu_localizations.dart
@@ -307,6 +307,606 @@ abstract class UbuntuLocalizations {
   /// In en, this message translates to:
   /// **'Shift'**
   String get shiftKey;
+
+  /// No description provided for @aboutLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'About'**
+  String get aboutLabel;
+
+  /// No description provided for @addLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Add'**
+  String get addLabel;
+
+  /// No description provided for @allLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'All'**
+  String get allLabel;
+
+  /// No description provided for @applyLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Apply'**
+  String get applyLabel;
+
+  /// No description provided for @ascendingLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Ascending'**
+  String get ascendingLabel;
+
+  /// No description provided for @backLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Back'**
+  String get backLabel;
+
+  /// No description provided for @boldLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Bold'**
+  String get boldLabel;
+
+  /// No description provided for @bottomLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Bottom'**
+  String get bottomLabel;
+
+  /// No description provided for @cancelLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Cancel'**
+  String get cancelLabel;
+
+  /// No description provided for @centerLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Center'**
+  String get centerLabel;
+
+  /// No description provided for @clearLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Clear'**
+  String get clearLabel;
+
+  /// No description provided for @closeLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Close'**
+  String get closeLabel;
+
+  /// No description provided for @colorLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Color'**
+  String get colorLabel;
+
+  /// No description provided for @connectLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Connect'**
+  String get connectLabel;
+
+  /// No description provided for @continueLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Continue'**
+  String get continueLabel;
+
+  /// No description provided for @copyLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Copy'**
+  String get copyLabel;
+
+  /// No description provided for @createLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Create'**
+  String get createLabel;
+
+  /// No description provided for @cutLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Cut'**
+  String get cutLabel;
+
+  /// No description provided for @decreaseLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Decrease'**
+  String get decreaseLabel;
+
+  /// No description provided for @deleteLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Delete'**
+  String get deleteLabel;
+
+  /// No description provided for @descendingLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Descending'**
+  String get descendingLabel;
+
+  /// No description provided for @discardLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Discard'**
+  String get discardLabel;
+
+  /// No description provided for @disconnectLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Disconnect'**
+  String get disconnectLabel;
+
+  /// No description provided for @doneLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Done'**
+  String get doneLabel;
+
+  /// No description provided for @downloadLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Download'**
+  String get downloadLabel;
+
+  /// No description provided for @editLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Edit'**
+  String get editLabel;
+
+  /// No description provided for @enterLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Enter'**
+  String get enterLabel;
+
+  /// No description provided for @errorLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Error'**
+  String get errorLabel;
+
+  /// No description provided for @executeLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Execute'**
+  String get executeLabel;
+
+  /// No description provided for @exitLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Exit'**
+  String get exitLabel;
+
+  /// No description provided for @fileLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'File'**
+  String get fileLabel;
+
+  /// No description provided for @fillLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Fill'**
+  String get fillLabel;
+
+  /// No description provided for @findLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Find'**
+  String get findLabel;
+
+  /// No description provided for @firstLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'First'**
+  String get firstLabel;
+
+  /// No description provided for @fontLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Font'**
+  String get fontLabel;
+
+  /// No description provided for @forwardLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Forward'**
+  String get forwardLabel;
+
+  /// No description provided for @fullscreenLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Fullscreen'**
+  String get fullscreenLabel;
+
+  /// No description provided for @goBackLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Go Back'**
+  String get goBackLabel;
+
+  /// No description provided for @helpLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Help'**
+  String get helpLabel;
+
+  /// No description provided for @homeLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Home'**
+  String get homeLabel;
+
+  /// No description provided for @importLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Import'**
+  String get importLabel;
+
+  /// No description provided for @increaseLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Increase'**
+  String get increaseLabel;
+
+  /// No description provided for @indexLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Index'**
+  String get indexLabel;
+
+  /// No description provided for @informationLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Information'**
+  String get informationLabel;
+
+  /// No description provided for @insertLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Insert'**
+  String get insertLabel;
+
+  /// No description provided for @italicLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Italic'**
+  String get italicLabel;
+
+  /// No description provided for @landscapeLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Landscape'**
+  String get landscapeLabel;
+
+  /// No description provided for @lastLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Last'**
+  String get lastLabel;
+
+  /// No description provided for @leaveLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Leave'**
+  String get leaveLabel;
+
+  /// No description provided for @leftLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Left'**
+  String get leftLabel;
+
+  /// No description provided for @mediaLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Media'**
+  String get mediaLabel;
+
+  /// No description provided for @networkLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Network'**
+  String get networkLabel;
+
+  /// No description provided for @newLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'New'**
+  String get newLabel;
+
+  /// No description provided for @nextLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Next'**
+  String get nextLabel;
+
+  /// No description provided for @noLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'No'**
+  String get noLabel;
+
+  /// No description provided for @noneLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'None'**
+  String get noneLabel;
+
+  /// No description provided for @normalLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Normal'**
+  String get normalLabel;
+
+  /// No description provided for @okLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'OK'**
+  String get okLabel;
+
+  /// No description provided for @openLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Open'**
+  String get openLabel;
+
+  /// No description provided for @pasteLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Paste'**
+  String get pasteLabel;
+
+  /// No description provided for @pauseLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Pause'**
+  String get pauseLabel;
+
+  /// No description provided for @playLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Play'**
+  String get playLabel;
+
+  /// No description provided for @portraitLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Portrait'**
+  String get portraitLabel;
+
+  /// No description provided for @preferencesLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Preferences'**
+  String get preferencesLabel;
+
+  /// No description provided for @previousLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Previous'**
+  String get previousLabel;
+
+  /// No description provided for @printLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Print'**
+  String get printLabel;
+
+  /// No description provided for @printPreviewLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Print Preview'**
+  String get printPreviewLabel;
+
+  /// No description provided for @propertiesLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Properties'**
+  String get propertiesLabel;
+
+  /// No description provided for @questionLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Question'**
+  String get questionLabel;
+
+  /// No description provided for @quitLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Quit'**
+  String get quitLabel;
+
+  /// No description provided for @recordLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Record'**
+  String get recordLabel;
+
+  /// No description provided for @redoLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Redo'**
+  String get redoLabel;
+
+  /// No description provided for @refreshLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Refresh'**
+  String get refreshLabel;
+
+  /// No description provided for @removeLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Remove'**
+  String get removeLabel;
+
+  /// No description provided for @renameLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Rename'**
+  String get renameLabel;
+
+  /// No description provided for @resetLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Reset'**
+  String get resetLabel;
+
+  /// No description provided for @restoreLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Restore'**
+  String get restoreLabel;
+
+  /// No description provided for @revertLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Revert'**
+  String get revertLabel;
+
+  /// No description provided for @rewindLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Rewind'**
+  String get rewindLabel;
+
+  /// No description provided for @rightLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Right'**
+  String get rightLabel;
+
+  /// No description provided for @saveAsLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Save As'**
+  String get saveAsLabel;
+
+  /// No description provided for @saveLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Save'**
+  String get saveLabel;
+
+  /// No description provided for @searchLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Search'**
+  String get searchLabel;
+
+  /// No description provided for @selectAllLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Select All'**
+  String get selectAllLabel;
+
+  /// No description provided for @selectLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Select'**
+  String get selectLabel;
+
+  /// No description provided for @sendLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Send'**
+  String get sendLabel;
+
+  /// No description provided for @sortLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Sort'**
+  String get sortLabel;
+
+  /// No description provided for @stopLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Stop'**
+  String get stopLabel;
+
+  /// No description provided for @strikeThroughLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Strike Through'**
+  String get strikeThroughLabel;
+
+  /// No description provided for @submitLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Submit'**
+  String get submitLabel;
+
+  /// No description provided for @topLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Top'**
+  String get topLabel;
+
+  /// No description provided for @undoLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Undo'**
+  String get undoLabel;
+
+  /// No description provided for @updateLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Update'**
+  String get updateLabel;
+
+  /// No description provided for @upLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Up'**
+  String get upLabel;
+
+  /// No description provided for @viewLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'View'**
+  String get viewLabel;
+
+  /// No description provided for @warningLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Warning'**
+  String get warningLabel;
+
+  /// No description provided for @windowLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Window'**
+  String get windowLabel;
+
+  /// No description provided for @yesLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Yes'**
+  String get yesLabel;
+
+  /// No description provided for @zoomInLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Zoom In'**
+  String get zoomInLabel;
+
+  /// No description provided for @zoomOutLabel.
+  ///
+  /// In en, this message translates to:
+  /// **'Zoom Out'**
+  String get zoomOutLabel;
 }
 
 class _UbuntuLocalizationsDelegate extends LocalizationsDelegate<UbuntuLocalizations> {
