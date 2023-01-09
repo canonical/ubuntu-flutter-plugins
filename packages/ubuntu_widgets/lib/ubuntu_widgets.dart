0acc7bae321ab39bee2a3d02edc7b877b6a68012
J-P Nurmi <jp.nurmi@canonical.com>
2023-01-09 16:12:57 +0100

    Remove `CheckButton`, `RadioButton` & `ToggleButton` (#119)
    Remove Check/Radio/ToggleButton
    
    Use YaruCheck/Radio/Switch/ToggleButton from Yaru widgets intead.
    
    Ref: canonical/ubuntu-desktop-installer#1223

--- packages/ubuntu_widgets/lib/ubuntu_widgets.dart
+++ packages/ubuntu_widgets/lib/ubuntu_widgets.dart
@@ -1,13 +1,9 @@
 /// A collection of widgets developed for Flutter-based Ubuntu applications.
 library ubuntu_widgets;
 
-export 'src/check_button.dart';
 export 'src/dropdown_builder.dart';
 export 'src/form_layout.dart';
 export 'src/key_search.dart';
 export 'src/log_view.dart';
 export 'src/menu_button_builder.dart';
-export 'src/radio_button.dart';
 export 'src/slide_show.dart';
-export 'src/toggle_button.dart';
-export 'src/toggle_button_theme.dart';
