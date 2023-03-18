546761f366e97a5f42b997691b0ccce2d4c0522c
J-P Nurmi <jp.nurmi@canonical.com>
2023-03-18 17:42:10 +0100

    Add support for PNG assets

--- packages/timezone_map/example/linux/my_application.cc
+++ packages/timezone_map/example/linux/my_application.cc
@@ -47,7 +47,7 @@ static void my_application_activate(GApplication* application) {
     gtk_window_set_title(window, "timezone_map_example");
   }
 
-  gtk_window_set_default_size(window, 1280, 720);
+  gtk_window_set_default_size(window, 960, 615);
   gtk_widget_show(GTK_WIDGET(window));
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();
