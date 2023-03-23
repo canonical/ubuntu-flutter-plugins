4852f94cbc3315c70eb9c58b20397d1aa0573cc3
Dennis Loose <dennis.loose@canonical.com>
2023-03-23 11:16:12 +0100

    geoip: fix duration
    Co-authored-by: J-P Nurmi <jp.nurmi@canonical.com>

--- packages/timezone_map/lib/src/geoip.dart
+++ packages/timezone_map/lib/src/geoip.dart
@@ -8,8 +8,8 @@ import 'location.dart';
 import 'source.dart';
 
 final _options = BaseOptions(
-  connectTimeout: const Duration(seconds: 5000),
-  receiveTimeout: const Duration(seconds: 5000),
+  connectTimeout: const Duration(seconds: 5),
+  receiveTimeout: const Duration(seconds: 5),
   responseType: ResponseType.plain,
 );
 
