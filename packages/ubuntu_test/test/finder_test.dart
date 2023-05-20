62d0c68afcb2656ac6ab2eadd2a516d974952b80
J-P Nurmi <jp.nurmi@canonical.com>
2023-05-20 12:34:39 +0200

    feat: import flutter_html & flutter_svg deps from yaru_test

---
+++ packages/ubuntu_test/test/finder_test.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ubuntu_test/ubuntu_test.dart';
+
+void main() {
+  testWidgets('image asset', (tester) async {
+    await tester.pumpWidget(Image.asset('assets/test.png'));
+    expect(find.asset('assets/test.png'), findsOneWidget);
+    expect(find.asset('assets/nothing.png'), findsNothing);
+  });
+
+  testWidgets('svg asset', (tester) async {
+    await tester.pumpWidget(SvgPicture.asset('assets/test.svg'));
+    expect(find.svg('assets/test.svg'), findsOneWidget);
+    expect(find.svg('assets/nothing.svg'), findsNothing);
+  });
+
+  testWidgets('html data', (tester) async {
+    await tester.pumpWidget(MaterialApp(home: Html(data: '<p>foo</p>')));
+    expect(find.html('<p>foo</p>'), findsOneWidget);
+    expect(find.html('<p>nothing</p>'), findsNothing);
+  });
+}
