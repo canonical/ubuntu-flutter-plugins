76670a42a26ba04ab2c7a38b635282f49c44a1f0
J-P Nurmi <jp.nurmi@canonical.com>
2023-07-04 16:24:21 +0200

    fix(ubuntu_localizations): fix formatByteSize(0)

--- packages/ubuntu_localizations/lib/src/data_size.dart
+++ packages/ubuntu_localizations/lib/src/data_size.dart
@@ -39,8 +39,10 @@ extension UbuntuDataSizeLocalizations on UbuntuLocalizations {
   String formatByteSize(num bytes, {int? precision}) {
     const divider = 1000;
     final units = [byte, kilobyte, megabyte, gigabyte, terabyte, petabyte];
-    final idx =
-        (math.log(bytes.abs()) ~/ math.log(divider)).clamp(0, units.length - 1);
+    final idx = bytes == 0
+        ? 0
+        : (math.log(bytes.abs()) ~/ math.log(divider))
+            .clamp(0, units.length - 1);
     final p = precision == null && (bytes < divider || bytes % divider == 0)
         ? 0
         : precision;
