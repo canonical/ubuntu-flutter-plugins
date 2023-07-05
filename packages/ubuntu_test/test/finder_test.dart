1c1a80124e777d4ef91f56fa539c38130617b2fd
Dennis Loose <dennis.loose@canonical.com>
2023-07-05 18:34:34 +0200

    feat(ubuntu_test): add markdownBody finder

--- packages/ubuntu_test/test/finder_test.dart
+++ packages/ubuntu_test/test/finder_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ubuntu_test/ubuntu_test.dart';
@@ -21,5 +22,12 @@ void main() {
     await tester.pumpWidget(MaterialApp(home: Html(data: '<p>foo</p>')));
     expect(find.html('<p>foo</p>'), findsOneWidget);
     expect(find.html('<p>nothing</p>'), findsNothing);
+  });
+
+  testWidgets('markdown data', (tester) async {
+    await tester
+        .pumpWidget(const MaterialApp(home: MarkdownBody(data: '**foo**')));
+    expect(find.markdownBody('**foo**'), findsOneWidget);
+    expect(find.markdownBody('**nothing**'), findsNothing);
   });
 }
