5323b8de2c112428a713caffef59ed4a414279d7
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2024-09-18 11:32:33 +0200

    chore: Fix formatting

--- packages/ubuntu_localizations/tool/date_time_symbols.dart
+++ packages/ubuntu_localizations/tool/date_time_symbols.dart
@@ -136,7 +136,7 @@ Reads the given CLDR file and prints the date and time symbols.
           (format) => format
               .findAllElements('pattern')
               .singleWhere(
-                  (pattern) => pattern.getAttribute('alt') != 'variant')
+                  (pattern) => pattern.getAttribute('alt') != 'variant',)
               .innerText
               .replaceAll('\'', '\\\''),
         )
