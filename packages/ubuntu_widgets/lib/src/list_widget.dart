620f3a35b13e1a3602b9d972bfd63394985a7e16
paul-geoghegan <paul.geoghegan@canonical.com>
2026-01-07 16:39:58 +0000

    fix(ListWidget)!: fix accessibility focus trap and remove tabFocusNode
    Previously, keyboard and screen reader users would get stuck on the internal focusNode. Navigation was restricted to the up and down arrows, which was not obvious to the user, and there was no accessible way to review the currently selected option or easily exit the list.
    
    This change wraps the ListWidget in a FocusScope with a key interceptor to improve navigation:
    
    - Users can now TAB and Shift+TAB out of the list to the next/previous focusable element (e.g., the "Next" button).
    
    - Arrow keys continue to function for internal list navigation.
    
    BREAKING CHANGE: The tabFocusNode parameter has been removed as it is no longer required for focus traversal.

--- packages/ubuntu_widgets/lib/src/list_widget.dart
+++ packages/ubuntu_widgets/lib/src/list_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:ubuntu_widgets/ubuntu_widgets.dart';
 import 'package:yaru/widgets.dart';
 
@@ -15,7 +16,6 @@ class ListWidget extends StatefulWidget {
     super.key,
     this.selectedIndex = -1,
     this.onKeySearch,
-    this.tabFocusNode,
     this.shrinkWrap = false,
   });
 
@@ -31,10 +31,6 @@ class ListWidget extends StatefulWidget {
   /// Called when a key is pressed.
   final ValueChanged<String>? onKeySearch;
 
-  /// This is the focus node for the next focus node outside of the list,
-  /// where the focus should go when you tab out of the list.
-  final FocusNode? tabFocusNode;
-
   /// Whether the list should shrink-wrap its contents or not.
   final bool shrinkWrap;
 
@@ -43,7 +39,6 @@ class ListWidget extends StatefulWidget {
 }
 
 class _ListWidgetState extends State<ListWidget> {
-  final _focusNode = FocusNode();
   final _scrollableKey = GlobalKey();
   ScrollController? _scrollController;
 
@@ -57,7 +52,6 @@ class _ListWidgetState extends State<ListWidget> {
 
   @override
   void dispose() {
-    _focusNode.dispose();
     _scrollController?.dispose();
     super.dispose();
   }
@@ -78,49 +72,76 @@ class _ListWidgetState extends State<ListWidget> {
       final center = tileOffset - viewHeight / 2 + _kTileHeight / 2;
       _scrollController?.jumpTo(center);
     }
-
-    _focusNode.requestFocus();
   }
 
   @override
   Widget build(BuildContext context) {
     return YaruBorderContainer(
       clipBehavior: Clip.antiAlias,
-      child: KeySearch(
-        autofocus: true,
-        focusNode: _focusNode,
-        tabFocusNode: widget.tabFocusNode,
-        onSearch: widget.onKeySearch ?? (_) => -1,
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            if (widget.itemCount <= 0 ||
-                constraints.maxWidth <= 0 ||
-                constraints.maxHeight <= 0) {
-              return const SizedBox.expand();
-            }
-            // calculate initial center-alignment
-            _scrollController ??= ScrollController(
-              initialScrollOffset: widget.selectedIndex * _kTileHeight -
-                  constraints.maxHeight / 2 +
-                  _kTileHeight / 2,
-            );
-            return FocusTraversalGroup(
-              policy: OrderedTraversalPolicy(),
-              child: ListView.builder(
-                key: _scrollableKey,
-                controller: _scrollController,
-                shrinkWrap: widget.shrinkWrap,
-                itemExtent: _kTileHeight,
-                itemCount: widget.itemCount,
-                itemBuilder: (context, index) => Builder(
-                  builder: (context) {
-                    if (index == widget.selectedIndex) {
-                      // bring a half-visible selected item into the viewport
-                      context.findRenderObject()?.showOnScreen();
+      // Wrap the list in a FocusScope to define it as a group in order to allow propper focus behaviour
+      child: FocusScope(
+        child: Builder(
+          builder: (context) {
+            return Focus(
+              // Intercept Key Events to handle Tab to Exit
+              onKeyEvent: (node, event) {
+                if (event is KeyDownEvent &&
+                    event.logicalKey == LogicalKeyboardKey.tab) {
+                  // Get the current scope (the list)
+                  final scope = FocusScope.of(context);
+
+                  // Move focus in the PARENT scope (The Page).
+                  // This jumps from the List to the Nex  or previous element
+                  // completely skipping the internal list items.
+                  if (HardwareKeyboard.instance.isShiftPressed) {
+                    scope.enclosingScope?.previousFocus();
+                  } else {
+                    scope.enclosingScope?.nextFocus();
+                  }
+
+                  return KeyEventResult.handled;
+                }
+
+                // Let Arrow keys pass through to the ListView naturally
+                return KeyEventResult.ignored;
+              },
+              child: KeySearch(
+                autofocus: false,
+                onSearch: widget.onKeySearch ?? (_) => -1,
+                child: LayoutBuilder(
+                  builder: (context, constraints) {
+                    if (widget.itemCount <= 0 ||
+                        constraints.maxWidth <= 0 ||
+                        constraints.maxHeight <= 0) {
+                      return const SizedBox.expand();
                     }
-                    return FocusTraversalOrder(
-                      order: NumericFocusOrder(index.toDouble()),
-                      child: widget.itemBuilder(context, index),
+                    // calculate initial center-alignment
+                    _scrollController ??= ScrollController(
+                      initialScrollOffset: widget.selectedIndex * _kTileHeight -
+                          constraints.maxHeight / 2 +
+                          _kTileHeight / 2,
+                    );
+                    return FocusTraversalGroup(
+                      policy: OrderedTraversalPolicy(),
+                      child: ListView.builder(
+                        key: _scrollableKey,
+                        controller: _scrollController,
+                        shrinkWrap: widget.shrinkWrap,
+                        itemExtent: _kTileHeight,
+                        itemCount: widget.itemCount,
+                        itemBuilder: (context, index) => Builder(
+                          builder: (context) {
+                            if (index == widget.selectedIndex) {
+                              // bring a half-visible selected item into the viewport
+                              context.findRenderObject()?.showOnScreen();
+                            }
+                            return FocusTraversalOrder(
+                              order: NumericFocusOrder(index.toDouble()),
+                              child: widget.itemBuilder(context, index),
+                            );
+                          },
+                        ),
+                      ),
                     );
                   },
                 ),
